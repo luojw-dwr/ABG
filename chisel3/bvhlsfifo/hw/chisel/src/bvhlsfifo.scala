35f0ce06256b3e150f9efe34a6a4bb2817051fd1
Jianwen LUO <luojw@shanghaitech.edu.cn>
2021-04-08 21:14:21 +0800

    now bvhlsfifo handles case(lat=0) more correctly

--- chisel3/bvhlsfifo/hw/chisel/src/bvhlsfifo.scala
+++ chisel3/bvhlsfifo/hw/chisel/src/bvhlsfifo.scala
@@ -24,28 +24,38 @@ class bvhlsfifo(
       Module(new SkidBuffer(UInt(bw.W), bvname))
   })
 
-  fifo.clk   := clk
-  fifo.reset := reset
-  fifo.if_read_ce  := if_read_ce
-  fifo.if_write_ce := if_write_ce
-  pipe map (x => {
-    x.clk := clk
-    x.reset := reset
-    x.ce := true.B
-  })
-
-  (pipe.slice(0, lat - 1) zip pipe.slice(1, lat)) map ({case (u, v) => {
-    u.dout <> v.din
-  }})
-
-  if_full_n := fifo.if_full_n
-  pipe(0).din.valid := fifo.if_empty_n
-  fifo.if_write := if_write
-  fifo.if_read := pipe(0).din.ready
-  fifo.if_din := if_din
-  pipe(0).din.bits := fifo.if_dout
-
-  pipe(lat - 1).dout.ready := if_read
-  if_dout := pipe(lat - 1).dout.bits
-  if_empty_n := pipe(lat - 1).dout.valid
+  if (lat > 0) {
+    fifo.clk   := clk
+    fifo.reset := reset
+    fifo.if_read_ce  := if_read_ce
+    fifo.if_write_ce := if_write_ce
+    pipe map (x => {
+      x.clk := clk
+      x.reset := reset
+      x.ce := true.B
+    })
+    (pipe.slice(0, lat - 1) zip pipe.slice(1, lat)) map ({case (u, v) => {
+      u.dout <> v.din
+    }})
+    if_full_n := fifo.if_full_n
+    pipe(0).din.valid := fifo.if_empty_n
+    fifo.if_write := if_write
+    fifo.if_read := pipe(0).din.ready
+    fifo.if_din := if_din
+    pipe(0).din.bits := fifo.if_dout
+    pipe(lat - 1).dout.ready := if_read
+    if_dout := pipe(lat - 1).dout.bits
+    if_empty_n := pipe(lat - 1).dout.valid
+  } else {
+    fifo.clk := clk
+    fifo.reset := reset
+    fifo.if_read_ce := if_read_ce
+    fifo.if_write_ce := if_write_ce
+    if_full_n := fifo.if_full_n
+    if_empty_n := fifo.if_empty_n
+    fifo.if_write := if_write
+    fifo.if_read := if_read
+    fifo.if_din := if_din
+    if_dout := fifo.if_dout
+  }
 }
