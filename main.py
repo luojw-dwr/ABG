3c884813edd4c1293be694fc21c7e7e5eb62ee89
Jianwen LUO <luojw@shanghaitech.edu.cn>
2021-04-13 14:32:28 +0800

    * added LatencyGraph for pipeline rebalance. * added transformation (induced by SlotGraph and Phi:DG->SG) from DataflowGraph to LatencyGraph.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from frontend.ParseCSV import *
 from transform.ModuleGraphToDataflowGraph import *
 from transform.GridGraphToSlotGraph import *
 from floorplan.AbgESolver import AbgESolver
+from transform.DataflowGraphToLatencyGraph import *
 
 import numpy as np
 
@@ -20,15 +21,14 @@ gg = parseGridCSV("assets/floorplan/U250.csv")
 logger.info("[Op Phase] Construct AbgE problem.")
 dg = moduleGraphToDataflowGraph(mg)
 sg = gridGraphToSlotGraph(gg)
+abgESolver = AbgESolver(dg, sg, defaultdict(lambda:0.85), None)
 
 logger.info("[Op Phase] Solve AbgE problem.")
-abgESolver = AbgESolver(dg, sg, defaultdict(lambda:0.85), None)
 PhiV, PhiE = abgESolver.solve()
-gMap = abgESolver.resolveMap(PhiV, PhiE)
+vMap, eMap = abgESolver.resolveMap(PhiV, PhiE)
 
-logger.info("[Op Phase] Store AbgE solution.")
-np.savetxt("build/PhiE.csv", PhiE, fmt="%d")
-np.savetxt("build/PhiV.csv", PhiV, fmt="%d")
+logger.info("[Op Phase] Construct LatRebal problem.")
+lg_raw = dataflowGraphToLatencyGraph(dg, sg, eMap)
 
 logger.info("[Op Phase] Generate optimized RTL.")
 vhandle.toCustomizedNames()
---
+++ pipeline/LatencyGraph.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class LatencyEdge:
+    def __init__(self, components, srcName, dstName, w, lat, bal):
+        self.components = components
+        self.srcName = srcName
+        self.dstName = dstName
+        self.w = w
+        self.lat = lat
+        self.bal = bal
+    def __repr__(self):
+        return f"LatencyEdge(components={self.components}, srcName={self.srcName}, dstName={self.dstName}, w={self.w}, lat={self.lat}, bal={self.bal})"
+
+class LatencyVertex:
+    def __init__(self, name):
+        self.name = name
+        self.sourceNames = deque()
+        self.sinkNames = deque()
+    def __repr__(self):
+        return f"LatencyVertex(name={self.name}, sourceNames={self.sourceNames}, sinkNames={self.sinkNames})"
+
+class LatencyGraph:
+    def __init__(self):
+        self.V = deque()
+        self.Vdict = {}
+        self.E = deque()
+        self.Edict = {}
+    def __repr__(self):
+        return f"LatencyGraph(V={self.V}, E={self.E})"
+    def addVertex(self, name):
+        v = LatencyVertex(name)
+        self.V.append(v)
+        self.Vdict[name] = v
+    def addEdge(self, comp, srcName, dstName, w, lat, bal):
+        e = LatencyEdge(comp, srcName, dstName, w, lat, bal)
+        self.E.append(e)
+        self.Edict[(srcName, dstName)] = e
+        self.Vdict[srcName].sinkNames.append(dstName)
+        self.Vdict[dstName].sourceNames.append(srcName)
