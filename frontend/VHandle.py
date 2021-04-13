076a0db596e6ef86070421a4571d54f43d0cbce0
Jianwen LUO <luojw@shanghaitech.edu.cn>
2021-04-13 13:52:04 +0800

    Improved logging behavior

--- frontend/VHandle.py
+++ frontend/VHandle.py
@@ -6,6 +6,8 @@ sys.setrecursionlimit(100000)
 
 import logging
 
+logger = logging.getLogger("ABG_VHandle")
+
 class VHandle:
     def __init__(self, vnode_root, vnodes_pe, vnodes_fifo, vnodes_fifoInst):
         self.vnode_root      = vnode_root
@@ -14,17 +16,17 @@ class VHandle:
         self.vnodes_fifo     = vnodes_fifo
         self.vnodes_fifoInst = vnodes_fifoInst
     def toCustomizedNames(self):
-        logging.info(f"Original Top Module Name: {self.vnode_moduleDef.name}")
+        logger.info(f"Original Top Module Name: {self.vnode_moduleDef.name}")
         self.vnode_moduleDef.name += "_postABG"
-        logging.info(f"Modified Top Module Name: {self.vnode_moduleDef.name}")
+        logger.info(f"Modified Top Module Name: {self.vnode_moduleDef.name}")
         for vnode_fifoInst in self.vnodes_fifoInst:
             vnode_fifo = next(filter(lambda node : isinstance(node, vast.Instance), vnode_fifoInst.children()))
             assert(vnode_fifoInst.module == vnode_fifo.module)
-            logging.info(f"Original FIFO Module Name for Instance {vnode_fifo.name}: {vnode_fifo.module}")
+            logger.info(f"Original FIFO Module Name for Instance {vnode_fifo.name}: {vnode_fifo.module}")
             vnode_fifoInst.module = vnode_fifo.name + "_M"
             vnode_fifo.module = vnode_fifo.name + "_M"
             assert(vnode_fifoInst.module == vnode_fifo.module)
-            logging.info(f"Modified FIFO Module Name for Instance {vnode_fifo.name}: {vnode_fifo.module}")
+            logger.info(f"Modified FIFO Module Name for Instance {vnode_fifo.name}: {vnode_fifo.module}")
     def toRTL(self):
         # may involve deep recursion...
         # due to impl. of pyverilog ASTCodeGenerator ...
--- main.py
+++ main.py
@@ -8,21 +8,32 @@ import numpy as np
 
 from collections import defaultdict
 
+import logging
+
+logger = logging.getLogger("ABG_main")
+logger.setLevel(logging.INFO)
+
+logger.info("[Op Phase] Parse assets.")
 vhandle, mg = parseTopV("assets/kernel3_u250/")
 gg = parseGridCSV("assets/floorplan/U250.csv")
 
+logger.info("[Op Phase] Construct AbgE problem.")
 dg = moduleGraphToDataflowGraph(mg)
 sg = gridGraphToSlotGraph(gg)
 
+logger.info("[Op Phase] Solve AbgE problem.")
 abgESolver = AbgESolver(dg, sg, defaultdict(lambda:0.85), None)
 PhiV, PhiE = abgESolver.solve()
 gMap = abgESolver.resolveMap(PhiV, PhiE)
-print(gMap)
 
+logger.info("[Op Phase] Store AbgE solution.")
 np.savetxt("build/PhiE.csv", PhiE, fmt="%d")
 np.savetxt("build/PhiV.csv", PhiV, fmt="%d")
 
+logger.info("[Op Phase] Generate optimized RTL.")
 vhandle.toCustomizedNames()
 top_module_name, rtl = vhandle.toRTL()
+
+logger.info("[Op Phase] Store optimized RTL.")
 with open(f"build/{top_module_name}.v", 'w') as f:
     f.write(rtl)
