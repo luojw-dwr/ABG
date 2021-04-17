8416b1c5fa2299f4f2b38a85c9c2031b650c7940
Jianwen LUO <luojw@shanghaitech.edu.cn>
2021-04-17 15:52:53 +0800

    revise hierPrefix in generation for PE floorplan constraints when already given with slash at the end

--- backend/AbgVMapToTcl.py
+++ backend/AbgVMapToTcl.py
@@ -1,4 +1,6 @@
 def abgVMapToTcl(hierPrefix, vMap):
+    if hierPrefix[-1] == "/":
+        hierPrefix = hierPrefix[:-1]
     return '\n'.join([
         f"add_cells_to_pblock [get_pblocks {v_SG.name} [get_cells -quiet [list {hierPrefix}/{v_DG.name}]]]"
         for (v_DG, v_SG) in vMap.items()
