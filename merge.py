36f9ea3fb1c17a544f8e76a7210aa30ba4c2a031
Jianwen LUO <luojw@shanghaitech.edu.cn>
2021-05-06 20:29:19 +0800

    added file copy and json modification in merge.py

---
+++ merge.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+top_name = "kernel3"
+proj_name = "kernel3_u250"
+proj_raw_path = "assets/kernel3_u250/"
+
+try:
+    os.makedirs("build/merged")
+except FileExistsError:
+    pass
+
+os.system(f"cp -r {proj_raw_path} build/merged")
+os.system(f"rm -r build/merged/{proj_name}/solution/syn/vhdl/")
+os.system(f"rm -r build/merged/{proj_name}/solution/impl/vhdl/")
+os.system(f"cp build/verilog/* build/merged/{proj_name}/solution/impl/verilog/")
+os.system(f"mv build/merged/{proj_name}/solution/impl/verilog/{top_name}_{top_name}_postABG.v build/merged/{proj_name}/solution/impl/verilog/{top_name}_{top_name}.v")
+os.system(f"sed -i 's/module*_postABG/module {top_name}_{top_name}/' build/merged/{proj_name}/solution/impl/verilog/{top_name}_{top_name}.v")
+
+with open(f"build/merged/{proj_name}/solution/solution_data.json", 'r') as f:
+    solution_data_json = json.loads(f.read())
+
+del solution_data_json["Files"]["Vhdl"]
+for vname in os.listdir("build/verilog/"):
+    solution_data_json["Files"]["Verilog"].append(f"impl/verilog/{vname}")
+
+with open(f"build/merged/{proj_name}/solution/solution_data.json", 'w') as f:
+    f.write(json.dumps(solution_data_json, indent=True))
+os.system(f"sed -i 's/\\//\\\\\\//g' build/merged/{proj_name}/solution/solution_data.json")
